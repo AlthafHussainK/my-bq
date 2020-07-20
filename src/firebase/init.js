f76e2a067a3028089d6a65fac671cfac65087967
MacBook Pro <macbookpro@Macs-MacBook-Pro.local>
2020-07-20 15:55:17 +0400

    Update firebaseConfig

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,14 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
-  authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  apiKey: "AIzaSyDNp071m0F8CaGEpEacVVowKON2Eu-gSRM",
+  authDomain: "my-bq-2k20.firebaseapp.com",
+  databaseURL: "https://my-bq-2k20.firebaseio.com",
+  projectId: "my-bq-2k20",
+  storageBucket: "my-bq-2k20.appspot.com",
+  messagingSenderId: "36458204012",
+  appId: "1:36458204012:web:569d87174cc9c034040149",
+  measurementId: "G-XV2SJ5TWHC"
 }
 
 firebase.initializeApp(config)
